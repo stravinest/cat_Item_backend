81fff3a484f995ffcff87466322dd5d4d744303f
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-10-11 15:57:12 +0900

    eslintrc 설정

--- app.js
+++ app.js
@@ -11,16 +11,15 @@ const port = process.env.EXPRESS_PORT;
 // 주소 형식으로 데이터를 보내는 방식
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'));// public에서 파일 참조 가능
+app.use(express.static('./public')); // public에서 파일 참조 가능
 
 // html을 대체하는 ejs 엔진을 설정
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
 app.use('/api', routers); // 라우터 폴더 적용
-app.use('/', renders);//
+app.use('/', renders); //
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
-
