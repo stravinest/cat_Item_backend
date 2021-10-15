4944188dbba4f71bcdc9e2eb969c222bc700a865
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-10-15 22:43:07 +0900

    jwt

--- routers/router_user_me.js
+++ routers/router_user_me.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 //const authMiddleware = require('../middlewares/auth_middleware');
 //const cors = require("cors");
 // router.use(cors())
-router.get('/me', async (req, res) => {
+router.post('/me', async (req, res) => {
   const token = req.body.token;
-  const { userId } = jwt.verify(token, process.env.SECRET_KEY);
-
+  console.log(token)
   if (token) {
+    console.log(token)
+    const { userId } = jwt.verify(token, process.env.SECRET_KEY);
     res.send({
       userId: userId,
     });
